a56fdf64138077917d518cdd441a8cd0147d63d3
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-06-07 01:01:50 -0500

    feature: added new shipment driver geolocation tracking component

---
+++ src/shipment-driver/pages/geolocation-tracking.component.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Hola</h1>
+</template>
+
+<script lang="ts">
+import { webSocketConnectionOptions } from '../../shared/services/ws-common';
+export default {
+    name: "geolocation-tracking",
+    data() {
+        return {
+            webSocket: new WebSocket(`${webSocketConnectionOptions.baseURL}/${this.id}`)
+        }
+    },
+    mounted() {
+        
+    },
+    props: {
+        id: Number
+    }
+}
+</script>
+
+<style>
+
+</style>
