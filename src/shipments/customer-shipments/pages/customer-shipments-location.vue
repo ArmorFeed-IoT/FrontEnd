41ea06fb37e93db768c358ecaf93368c29d83099
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-06-28 18:33:33 -0500

    fix: fixed customer shipments location component

--- src/shipments/customer-shipments/pages/customer-shipments-location.vue
+++ src/shipments/customer-shipments/pages/customer-shipments-location.vue
@@ -1,18 +1,15 @@
 <template>
   <GMapMap
-    :center="center"
+    :center="position"
     :zoom="16"
     map-type-id="terrain"
     style="width: 75vw; height: 25rem"
   >
     <GMapCluster>
       <GMapMarker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
+        :position="position"
         :clickable="true"
         :draggable="true"
-        @click="center=m.position"
       />
     </GMapCluster>
   </GMapMap>
@@ -23,24 +20,14 @@ export default {
   name: "customer-shipments-location",
   data() {
     return {
-      center: {lat: -12.090795, lng: -77.023148},
-      markers: [
-        {
-          position: {
-            lat: -12.090795,
-            lng: -77.023148
-          },
-        },
-      ],
-
-      locationService: null,
-      location: [],
-      columns: [
-        {field: 'lat', header: 'Lat'},
-        {field: 'lng', header: 'Lng'},
-      ],
-      currentLocation: [],
-
+      socketConnection: null,
+      position: {
+        lat: this.$dataTransfer.locationData.latitude,
+        lng: this.$dataTransfer.locationData.longitude
+      },
+      latitude: this.$dataTransfer.locationData.latitude,
+      longitude: this.$dataTransfer.locationData.longitude,
+      height: this.$dataTransfer.locationData.height
     }
   },
   mounted() {
@@ -64,6 +51,9 @@ export default {
         console.log(data);
     }
   },
+  props: {
+    customerId: Number
+  }
 
 }
 </script>
